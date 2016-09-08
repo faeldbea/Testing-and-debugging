84f0521bf4b2c10eed2e7d73585ce400d44e6865
faeldbea <fedealba068@gmail.com>
2016-09-08 12:11:22 -0300

    Create Fuzz-testing.py

---
+++ Fuzz-testing.py
@@ -0,0 +1,45 @@
+# Fuzz Testing
+# ------------
+# Write a random fuzzer, based on Charlie Miller's example
+# from Problem Set 4, for a text viewer application.
+#
+# For multiple iterations, the procedure, fuzzit, should take in the content
+# of a text file, pass the content into a byte array, randomly modify bytes
+# of the "file", and add the resulting byte array (as a String) to a list. 
+# The return value of the fuzzit procedure should be a list of 
+# byte-modified strings.
+
+
+import random
+import math
+
+content = """
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Phasellus sollicitudin condimentum libero,
+sit amet ultrices lacus faucibus nec.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Cum sociis natoque penatibus et magnis dis parturient montes,
+nascetur ridiculus mus. Cras nulla nisi, accumsan gravida commodo et,
+venenatis dignissim quam. Mauris rutrum ullamcorper consectetur.
+Nunc luctus dui eu libero fringilla tempor. Integer vitae libero purus.
+Fusce est dui, suscipit mollis pellentesque vel, cursus sed sapien.
+Duis quam nibh, dictum ut dictum eget, ultrices in tortor.
+In hac habitasse platea dictumst. Morbi et leo enim.
+Aenean ipsum ipsum, laoreet vel cursus a, tincidunt ultrices augue.
+Aliquam ac erat eget nunc lacinia imperdiet vel id nulla."""
+
+
+def fuzzit(content):
+	f = []
+	n = 50
+	for i in range(n):
+		byte_array = bytearray(content, 'ascii')
+		iterations = random.randint(0, 2*len(byte_array))
+		for iteration in range(iterations):
+			index_to_modify = random.randint(0, len(byte_array)-1)
+			replacement = random.randint(0, 255)
+			byte_array[index_to_modify] = replacement
+		f.append(str(byte_array))
+	return f
+# Write a random fuzzer for a simulated text viewer application
+fuzzit(content)
